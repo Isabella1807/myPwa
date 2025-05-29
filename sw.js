d924a6f139546189291a7e0cdabf12dacdc5a799
Isabella1807 <megaoreomuffin@hotmail.com>
2025-05-29 18:21:27 +0200

    fallback works

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-//Gemmer ting der ikke ændre sig særlig ofte.
-const staticCache = 'staticCache-v15'
-const dynamicCache = 'dynamicCache-v10'
+const staticCache = 'staticCache-v1'
+const dynamicCache = 'dynamicCache-v1'
 
 
 const assets = [
@@ -9,25 +8,15 @@ const assets = [
     '/js/app.js',
     '/css/main.css',
     '/js/nav.js',
-    '/img/icons/icon48.png',
-    '/img/icons/icon72.png',
-    '/img/icons/icon96.png',
-    '/img/icons/icon144.png',
-    '/img/icons/icon192.png',
-    '/img/icons/icon512.png',
-    '/img/icons/icon1024.png',
     '/pages/test.html',
 ];
 
 //Kører når en serviceWorker installeres i browseren, hvis nødvendigt.
 self.addEventListener('install', event => {
-    //console.log('Service worker install', event)
-    //Der er en chance for at vi ikke er færdige med at cache før install eventet slutter
+    console.log('Service worker install', event)
     event.waitUntil(
-        //åbner cachen, hvis staticCache ikke eksistere så opret og fortsæt
+        //hvis staticCache ikke eksistere så opret og fortsæt
         caches.open(staticCache).then(cache => {
-            //her tilføjer vi ting til cachen
-            //console.log('cachiiing');
             cache.addAll(assets)
         })
     );
@@ -35,12 +24,8 @@ self.addEventListener('install', event => {
 
 //kør nyeste sw, slet den gamle og ryd op i cache
 self.addEventListener('activate', event => {
-    //console.log('Service worker activate', event)
     event.waitUntil(
-        //asynkron der returnere et promise, og går igennem cache og leder efter keys.
-        //Keys returnere en liste af caches.
         caches.keys().then(keys => {
-            //console.log(keys);
             return Promise.all(keys
                 //Alt der skal slettes, bliver i listen, som så slettes.
                 .filter(key => key !== staticCache && key !== dynamicCache)
@@ -52,9 +37,7 @@ self.addEventListener('activate', event => {
 
 //fetch intercepts requests til serveren
 self.addEventListener('fetch', event => {
-
     console.log('Request', event.request.url);
-    //pauser fetch eventet og svarer med vores eget respons, som er fra cachen.
     event.respondWith(
         caches.match(event.request).then(cacheRes => {
             //Hvis der ikke er noget i cachen (cacheRes), så returner det originale request
@@ -64,12 +47,24 @@ self.addEventListener('fetch', event => {
                     return fetchRes
                 })
             });
+        }).catch(() => caches.match('/pages/test.html'))
+    )
+
+/*    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            fetch(event.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(event.request.url, fetchRes.clone());
+                    return fetchRes
+                })
+            });
+
+            return cacheRes;
         }).catch(() => {
-                //hvis
                 if (event.request.url.indexOf('.html') > -1) {
                     caches.match('/pages/test.html')
                 }
             }
         )
-    )
+    )*/
 });
